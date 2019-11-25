3d0307bc29a8cb7bb24f2823ee4c9b6e983d615b
wushuaishuai <wushuaishuai@zuoyebang.com>
2019-11-25 10:55:52 +0800

    update

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/FengGeSe/nest
 
 go 1.13
+
+require github.com/rakyll/statik v0.1.6
--- main.go
+++ main.go
@@ -1,8 +1,31 @@
 //go:generate statik -src=./tpl -f
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/rakyll/statik/fs"
+
+	_ "github.com/FengGeSe/nest/statik"
+)
 
 func main() {
-	fmt.Println("vim-go")
+
+	statikFS, err := fs.New()
+	if err != nil {
+		panic(err)
+	}
+
+	if err := fs.Walk(statikFS, "/", Show); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("end")
+}
+
+func Show(path string, info os.FileInfo, err error) error {
+
+	fmt.Println(path, info.Name(), err)
+	return nil
 }
